7f67493f4593fe80678db4e1687c41bb64458e1a
Siddhartha Singh <siddharthagithub0007@gmail.com>
2026-07-02 13:27:34 +0530

    Revert "test: speed up TestVersionUpgrade to avoid 10m timeout on CI"
    This reverts commit 1fff305e1489abe4d5af361ca1b0439fff0077c5.
    
    Signed-off-by: Siddhartha Singh <siddharthagithub0007@gmail.com>

--- cmd/prometheus/main_upgrade_test.go
+++ cmd/prometheus/main_upgrade_test.go
@@ -248,15 +248,15 @@ rule_files:
 
 scrape_configs:
   - job_name: 'self1'
-    scrape_interval: 100ms
+    scrape_interval: 1s
     static_configs:
       - targets: ['localhost:%d']
   - job_name: 'self2'
-    scrape_interval: 200ms
+    scrape_interval: 2s
     static_configs:
       - targets: ['localhost:%d']
   - job_name: 'self3'
-    scrape_interval: 300ms
+    scrape_interval: 3s
     static_configs:
       - targets: ['localhost:%d']
 
@@ -357,8 +357,7 @@ func testUpgradeDowngradeLTS(t *testing.T, start time.Time, lts ltsRelease) {
 		fmt.Sprintf("--web.listen-address=0.0.0.0:%d", c.prometheusPort),
 		fmt.Sprintf("--storage.tsdb.path=%s", c.prometheusDataPath),
 		// Accelerate compaction.
-		"--storage.tsdb.min-block-duration=15s",
-		"--storage.tsdb.max-block-duration=15s",
+		"--storage.tsdb.min-block-duration=65s",
 		// Accelerate chunks mmapping.
 		"--storage.tsdb.samples-per-chunk=10",
 		"--log.level=debug",
